ff013c069c674d40f694cac34a97973287d4bec5
“Edy <csmcorporatama@gmail.com>
2023-02-06 16:47:00 +0700

    first commit

--- README.md
+++ README.md
@@ -1,46 +1,13 @@
-# Getting Started with Create React App
+Query Parameterizer
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This is a script that allows you to input a query and a set of parameters to format the query, and returns a parameterized version of the query. The parameterized query can then be easily used in a database application. Usage
 
-## Available Scripts
+Paste your query in the left text area.
+Split the query from the parameters using ||.
+Format the parameters according to the script and paste them after the split.
+The output is automatically copied to your clipboard and also displayed in the right text area.
+Requirements
 
-In the project directory, you can run:
+This script uses JavaScript and is designed to run in a browser environment. Contributing
 
-### `npm start`
-
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
+Feel free to make a pull request if you have any suggestions for improvement.
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,161 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+export default function App() {
+  const [showAlert, setShowAlert] = useState(false);
+
+  function onPasteQuery() {
+    setTimeout(() => {
+      var query = (document.getElementById('txtQuery') as HTMLInputElement).value;
+      (document.getElementById('txtResult') as HTMLInputElement).value = '';
+      let input = query;
+      let outputQuery = input.split('!!')[0];
+      let outputParam = 'return ' + input.split('return')[1].trim();
+      let outputParamItems = 'q';
+      if (!input.includes('!!')) {
+        alert('mohon tambah split dengan menambahkan !! di akhir query');
+        return;
+      }
+      let paramsQuery = input.split('!!')[1].split(/\r?\n/);
+      let i = 0;
+      for (let o of paramsQuery) {
+        if (o.trim() === '' || o.trim().includes('return')) {
+          continue;
+        }
+        let param = o.replaceAll(' ', '');
+        let containsPercent = false;
+        if (o.includes("'{0}'")) {
+          param = param
+            .replaceAll('q=q.Replace("', '')
+            .replaceAll('",string.Format("\'{0}\'"', '')
+            .replaceAll('));', '')
+            .trim();
+        } else if (o.includes("'%{0}%'")) {
+          param = param
+            .replaceAll('q=q.Replace("', '')
+            .replaceAll('",string.Format("\'%{0}%\'"', '')
+            .replaceAll('));', '')
+            .trim();
+          containsPercent = true;
+        } else if (o.includes('{0}')) {
+          param = param
+            .replaceAll('q=q.Replace("', '')
+            .replaceAll('",string.Format("{0}"', '')
+            .replaceAll('));', '')
+            .trim();
+        } else {
+          param = param
+            .replaceAll('q=q.Replace("', '')
+            .replaceAll('",', ',')
+            .replaceAll(');', '')
+            .trim();
+        }
+        let paramSplit = param.split(',');
+        outputQuery = outputQuery.replaceAll("'" + paramSplit[0].trim() + "'", '@' + i.toString());
+        outputQuery = outputQuery.replaceAll(paramSplit[0].trim(), '@' + i.toString());
+        outputParamItems +=
+          '\r\n' +
+          ',' +
+          (!containsPercent
+            ? paramSplit[1].trim()
+            : 'string.Format("%{0}%", ' + paramSplit[1].trim() + ')');
+        i++;
+      }
+      var output =
+        outputQuery + '\r\n' + outputParam.replaceAll('(q)', '(' + outputParamItems + ')');
+      navigator.clipboard.writeText(output);
+      (document.getElementById('txtQuery') as HTMLInputElement).value = '';
+      (document.getElementById('txtResult') as HTMLInputElement).value = output;
+      setShowAlert(true);
+      setTimeout(() => {
+        setShowAlert(false);
+      }, 4000);
+    }, 500);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='w-full h-full flex flex-row relative'>
+      <div
+        className={
+          'alert alert-success shadow-lg absolute top-0 right-0 w-[300px] ' +
+          (showAlert ? 'block' : 'hidden')
+        }
+      >
+        <div>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className='stroke-current flex-shrink-0 h-6 w-6'
+            fill='none'
+            viewBox='0 0 24 24'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth='2'
+              d='M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z'
+            />
+          </svg>
+          <span>Copy Success</span>
+        </div>
+      </div>
+      <div className='w-full h-full flex flex-row'>
+        <div className='flex-1  px-[50px] py-[50px]'>
+          <div className='mockup-code'>
+            <pre>
+              <code>Example Unparameterized Query</code>
+            </pre>
+            <pre>
+              <code>string q = {'""'};</code>
+            </pre>
+            <pre>
+              <code>q += {'"SELECT * FROM MSUSER a WHERE a.UserID = @UserID"'};</code>
+            </pre>
+            <pre>
+              <code>!!</code>
+            </pre>
+            <pre>
+              <code>q = {'q.Replace("@UserID", userID)'};</code>
+            </pre>
+            <pre>
+              <code>return MdlDtl.RetrieveListDictionaryQuery(q);</code>
+            </pre>
+          </div>
+          <textarea
+            id='txtQuery'
+            className='textarea bg-gray-600 w-full h-full mt-5'
+            placeholder='Query'
+            onPaste={() => {
+              onPasteQuery();
+            }}
+          ></textarea>
+        </div>
+        <div className='flex-1  px-[50px] py-[50px]'>
+          <div className='mockup-code'>
+            <pre>
+              <code>Example Parameterized Query</code>
+            </pre>
+            <pre>
+              <code>string q = {'""'};</code>
+            </pre>
+            <pre>
+              <code>q += {'"SELECT * FROM MSUSER a WHERE a.UserID = @0"'};</code>
+            </pre>
+            <pre>
+              <code></code>
+            </pre>
+            <pre>
+              <code>return MdlDtl.RetrieveListDictionaryQuery(q</code>
+            </pre>
+            <pre>
+              <code>, userID);</code>
+            </pre>
+          </div>
+          <textarea
+            id='txtResult'
+            className='textarea bg-gray-600 w-full h-full mt-5'
+            placeholder='Parameterized'
+          ></textarea>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
